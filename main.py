366433c7639bcc63d72506365b31089b2842ae1d
rithik <rithikmotupalli@gmail.com>
2024-09-05 23:30:00 +0530

    changed the default values of the input box to not have any loading errors

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import streamlit as st
 
 
 #code for creating a UI using streamlit
-s = st.number_input("Enter the stock price : ")
-x = st.number_input("Enter the strike price : ")
-r = st.number_input("Enter the interest rate : ")
-t = st.number_input("Enter the time to expiration : ")
-sigma = st.number_input("Enter the volatility : ")
+s = st.number_input("Enter the stock price : ",value = 1)
+x = st.number_input("Enter the strike price : ",value=1)
+r = st.number_input("Enter the interest rate : ",value = 0.01)
+t = st.number_input("Enter the time to expiration : ",value = 1)
+sigma = st.number_input("Enter the volatility : ",value = 0.1)
 
 
 
