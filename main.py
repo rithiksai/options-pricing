8dd2560c0fa4d89ed0abd07f0e5b918d5ca52292
rithik <rithikmotupalli@gmail.com>
2024-09-05 23:03:37 +0530

    all calculation code added successfully

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
+from statistics import NormalDist
+
 
 s = float(input("Enter the stock price : "))
 x = float(input("Enter the strike price : "))
@@ -22,3 +24,15 @@ d1 =calcd1(s,x,r,t,sigma)
 d2 = calcd2(d1,sigma,t)
 print(d1)
 print(d2)
+
+
+nd1 = NormalDist(mu=0, sigma=1).cdf(d1)
+nd2 = NormalDist(mu=0, sigma=1).cdf(d2)
+
+print(nd1)
+print(nd2)
+
+z = r*t
+c = (s*nd1)- (x*math.exp(-z)*nd2)
+
+print("THE PRICE OF THE EUROPEAN CALL OPOTION : "+ str(c))
