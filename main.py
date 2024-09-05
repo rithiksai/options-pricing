26fbeaf0f7946eda5256d78173482c7dd4ae6b57
rithik <rithikmotupalli@gmail.com>
2024-09-05 23:23:18 +0530

    added a basic working UI with eroor

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import math
 from statistics import NormalDist
+import streamlit as st
 
 
-s = float(input("Enter the stock price : "))
-x = float(input("Enter the strike price : "))
-r = float(input("Enter the interest rate : "))
-t = float(input("Enter the time to expiration : "))
-sigma = float(input("Enter the volatility : "))
+
+#code for creating a UI using streamlit
+s = st.number_input("Enter the stock price : ")
+x = st.number_input("Enter the strike price : ")
+r = st.number_input("Enter the interest rate : ")
+t = st.number_input("Enter the time to expiration : ")
+sigma = st.number_input("Enter the volatility : ")
+
+
 
 def calcd1(s,x,r,t,sigma):
     a =  math.log(s/x)
@@ -22,17 +27,14 @@ def calcd2(d1,sigma,t):
 
 d1 =calcd1(s,x,r,t,sigma)
 d2 = calcd2(d1,sigma,t)
-print(d1)
-print(d2)
+
 
 
 nd1 = NormalDist(mu=0, sigma=1).cdf(d1)
 nd2 = NormalDist(mu=0, sigma=1).cdf(d2)
 
-print(nd1)
-print(nd2)
 
 z = r*t
 c = (s*nd1)- (x*math.exp(-z)*nd2)
 
-print("THE PRICE OF THE EUROPEAN CALL OPOTION : "+ str(c))
+st.write("THE PRICE OF THE EUROPEAN CALL OPOTION : ",c)
