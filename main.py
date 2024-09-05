73fffea48e746c531405e0392b6ea7c06bd762a3
rithik <rithikmotupalli@gmail.com>
2024-09-05 22:51:42 +0530

    addedcode to the main file

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world`")
+import math
+
+s = float(input("Enter the stock price : "))
+x = float(input("Enter the strike price : "))
+r = float(input("Enter the interest rate : "))
+t = float(input("Enter the time to expiration : "))
+sigma = float(input("Enter the volatility : "))
+
+def calcd1(s,x,r,t,sigma):
+    a =  math.log(s/x)
+    b = (sigma**2/2) + r
+    c = sigma*math.sqrt(t)
+    d1 = (a + b*t)/c
+    return d1
+
+def calcd2(d1,sigma,t):
+    
+    d2 = d1 - (sigma*math.sqrt(t))
+    return d2
+
+d1 =calcd1(s,x,r,t,sigma)
+d2 = calcd2(d1,sigma,t)
+print(d1)
+print(d2)
