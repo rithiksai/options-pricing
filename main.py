5f053909220ff4e794473df2ba76010c70e3ef26
Rithik <rithikmotupalli@gmail.com>
2024-09-06 16:11:38 +0530

    added an enter button

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import math
 from statistics import NormalDist
 import streamlit as st
 
-
-
+st.title(":sunglasses: :blue[call] and :green[put] option price calculator")
 #code for creating a UI using streamlit
 s = st.number_input("Enter the stock price : ",value = 1)
 x = st.number_input("Enter the strike price : ",value=1)
@@ -12,29 +11,45 @@ t = st.number_input("Enter the time to expiration : ",value = 1)
 sigma = st.number_input("Enter the volatility : ",value = 0.1)
 
 
+if st.button("Calculate"):
+    def calcd1(s,x,r,t,sigma):
+        a =  math.log(s/x)
+        b = (sigma**2/2) + r
+        c = sigma*math.sqrt(t)
+        d1 = (a + b*t)/c
+        return d1
 
-def calcd1(s,x,r,t,sigma):
-    a =  math.log(s/x)
-    b = (sigma**2/2) + r
-    c = sigma*math.sqrt(t)
-    d1 = (a + b*t)/c
-    return d1
+    def calcd2(d1,sigma,t):
+        
+        d2 = d1 - (sigma*math.sqrt(t))
+        return d2
 
-def calcd2(d1,sigma,t):
+    d1 =calcd1(s,x,r,t,sigma)
+    d2 = calcd2(d1,sigma,t)
+
+
+
+    nd1 = NormalDist(mu=0, sigma=1).cdf(d1)
+    nd2 = NormalDist(mu=0, sigma=1).cdf(d2)
+
+
+    z = r*t
+    c = (s*nd1)- (x*math.exp(-z)*nd2)
+
+
+    font_size = 30
+
+    html_str = f"""
+    <style>
+    p.a {{
+    font: bold {font_size}px Courier;
+    color:green;
     
-    d2 = d1 - (sigma*math.sqrt(t))
-    return d2
+    }}
+    </style>
+    <p class="a">CALL : {c}</p>
+    """
 
-d1 =calcd1(s,x,r,t,sigma)
-d2 = calcd2(d1,sigma,t)
-
-
-
-nd1 = NormalDist(mu=0, sigma=1).cdf(d1)
-nd2 = NormalDist(mu=0, sigma=1).cdf(d2)
+    st.markdown(html_str, unsafe_allow_html=True)
 
 
-z = r*t
-c = (s*nd1)- (x*math.exp(-z)*nd2)
-
-st.write("THE PRICE OF THE EUROPEAN CALL OPOTION : ",c)
